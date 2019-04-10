c485c73b8b12eecf3142923e40d7f1eff6ea8923
Elton Martins <elton@devell.com.br>
2019-04-10 19:24:40 -0300

    teste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,118 @@
+declare let audioinput: any;
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	audioDataBuffer: any[];
+	totalReceivedData: number = 0;
 
-  constructor(public navCtrl: NavController) {
+  	constructor(public navCtrl: NavController, public platform: Platform) {
+  		console.log('construtor')
+  	}
 
-  }
+  	ionViewDidLoad () {
+		console.log("Load!");
+		this.platform.ready().then(() => {
+			console.log('Ready')
 
+			// SAMUEL
+			// É isso aqui que eu estava testando quando parou de funcionar
+			window.addEventListener('audioinput', this.onAudioInputCapture, false);
+			window.addEventListener('audioinputerror', this.onAudioInputError, false);
+		});
+	}
+
+	onAudioInputCapture(evt) {
+		console.log('capture')
+		console.log(evt.data)
+	    try {
+	        if (evt && evt.data) {
+	            // Increase the debug counter for received data
+	            totalReceivedData += evt.data.length;
+
+	            // Add the chunk to the buffer
+	            audioDataBuffer = audioDataBuffer.concat(evt.data);
+	        }
+	    }
+	    catch (ex) {
+	        alert("onAudioInputCapture ex: " + ex);
+	    }
+	}
+
+
+	/**
+	 * Called when a plugin error happens.
+	 */
+	onAudioInputError(error) {
+	    alert("onAudioInputError event recieved: " + JSON.stringify(error));
+	}
+
+  	record() {
+		if (audioinput.isCapturing())
+			return;
+
+		let options = {
+			sampleRate: 44100,
+			bufferSize: 16384,
+			channels: 2, // 1 - Mono, 2 - Stereo
+			format: audioinput.FORMAT.PCM_16BIT,
+			audioSourceType: audioinput.AUDIOSOURCE_TYPE.VOICE_COMMUNICATION,
+			audioContext: this.audioContext
+		};
+
+		audioinput.initialize(options, () => {
+			audioinput.checkMicrophonePermission((hasPermission) => {
+	            if(hasPermission){
+	                console.warn('Já tem permissão para acessar o microfone');
+	                this.start();
+	            } else {
+	                console.warn('Sem permissão para acessar o microfone');
+	                console.warn('Perguntando...');
+	                audioinput.getMicrophonePermission((hasPermission, message) => {
+	                    if(hasPermission) {
+	                        console.warn('Usuário permitiu');
+	                        this.start();
+	                    } else {
+	                        console.warn('Usuário rejeitou');
+	                    }
+	                });
+	            }
+	        });
+			console.log('init');
+		})
+	}
+
+  	start () {
+  		if (audioinput) {
+  			console.log('start')
+			audioinput.start({streamToWebAudio: true});
+		}
+  	}
+
+  	stop() {
+		try {
+			if (audioinput) {
+        		if (audioinput.isCapturing()) {
+					console.log("Stop");
+                	audioinput.stop();
+
+                	// !!!!!!!!!!!!! AQUI !!!!!!!!!!!
+                	// Precisamos gravar o arquivo e enviar...
+            	}
+        	}
+    	}
+    	catch (e) {
+        	alert("Erro: " + e);
+    	}
+  	}
+
+  	play() {
+  		console.log('Reproduzir');
+  	}
 }
